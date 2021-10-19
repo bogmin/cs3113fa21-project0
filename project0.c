6e022d5c1e6adf876bd5b92ef50defd26dec6951
bogmin <55713690+bogmin@users.noreply.github.com>
2021-10-19 18:45:49 -0500

    ss

--- project0.c
+++ project0.c
@@ -38,9 +38,9 @@ hashBlock * search(char * key) {    //searches for a given key
     uint64_t ind = HASH_CODE(key_int); //get a valid index from the provided key
 
     //move in array until an empty
-    while(hashArray[ind] != NULL) {
+    while (hashArray[ind] != NULL) {
 
-        if (memcmp(hashArray[ind]->key, key, 5) == 0){
+        if (memcmp(hashArray[ind]->key, key, 5) == 0) {
             return hashArray[ind];
         }
 
@@ -50,22 +50,6 @@ hashBlock * search(char * key) {    //searches for a given key
 
     return NULL;
 }
-
-
-/*void bubbleSort(int i){ //sorts hashArray for printing
-    int j, k;
-    hashBlock* temp;
-
-    for (j = 0; j < i - 1; j++){
-        for (k = 0; k < (i - 1 - j); k++){
-            if (hashArray[k]->data < hashArray[k + 1]->data){
-                temp = hashArray[k];
-                hashArray[k] = hashArray[k+1];
-                hashArray[k+1] = temp;
-            }
-        }
-    }
-}*/
 
 int insert(char * key, int data, int counter) {     //slap them values in yeehaw
     //allocate memory for the new item in the hash array
@@ -104,17 +88,10 @@ void print() {	//prints out the hash table in a sorted manner
         if (hashArray[i] != NULL) {
             printf("%s->%d\n", hashArray[i]->key, hashArray[i]->data.num_occurrences);
         }
-        /*
-        else {
-			printf("Program completed...");
-			break;
-			break;
-		}*/
-
     }
 }
 
-int comp (const hashBlock * ele1, const hashBlock * ele2){
+int comp (const void * ele1, const void * ele2){
     if (ele1->data.num_occurrences > ele2->data.num_occurrences){
         return 1;
     }
@@ -145,28 +122,6 @@ int main() {
     while (!feof(stdin)){
         memset(c, 0, sizeof(c)); //clear the previous contents of c
 
-        /*   scanf("%c", &c[0]);
-            if ((c[0] & 0x80) == 0) { //this unicode character is only one byte
-                valid_bytes = 1;
-
-            }
-            else if ((c[0] & 0xC0) == 0x80) { //this unicode character is two bytes
-                scanf("%c", &c[1]);
-                valid_bytes = 2;
-
-            }
-            else if ((c[0] & 0xE0) == 0xC0) { //this unicode character is three bytes
-                scanf("%c", &c[1]);
-                scanf("%c", &c[2]);
-                valid_bytes = 3;
-            }
-            else if ((c[0] & 0xF0) == 0xE0) { //this unicode character is four bytes
-                scanf("%c", &c[1]);
-                scanf("%c", &c[2]);
-                scanf("%c", &c[3]);
-                valid_bytes = 4;
-            */
-
         scanf("%c",&c[0]);
         if ((int) c[0] > 239){
             scanf("%3s", &c[1]);
@@ -177,9 +132,6 @@ int main() {
         else if((int) c[0] > 191){
             scanf("%c", &c[1]);
         }
-        // else { //this is an error case
-        //   printf("Error: invalid unicode byte detected\n");
-        //}
 
         item = search(c);
         if (item != NULL) {
