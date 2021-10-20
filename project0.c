ec840d4f4fefb2a775e97b580efb3cdbb41befc3
bogmin <55713690+bogmin@users.noreply.github.com>
2021-10-19 20:18:10 -0500

    Update project0.c

--- project0.c
+++ project0.c
@@ -51,35 +51,29 @@ hashBlock * search(char * key) {    //searches for a given key
     return NULL;
 }
 
-int insert(char * key, int data) {     //slap them values in yeehaw
+void insert(char * key, data_t * data) {     //slap them values in yeehaw
     //allocate memory for the new item in the hash array
     hashBlock * new_item = malloc(sizeof(hashBlock));
-    int counter = 0;
     //  int counter = 0; //track number of occupied cells for sorting
 
     //copy the data passed in, into the new item
-    new_item->data.num_occurrences = data;
+    memcpy(&new_item->data, data, sizeof(data_t));
     memcpy(new_item->key, key, 5);
 
-    //get the hash index
-    uint64_t key_int = key_to_int(key); //turn the key into an integer representation
-    uint64_t ind = HASH_CODE(key_int); //get the hash index for this key value
+   //get the hash index
+   uint64_t key_int = key_to_int(key); //turn the key into an integer representation
+   uint64_t ind = HASH_CODE(key_int); //get the hash index for this key value
 
     //move in array until an empty or deleted cell
-    while(true){
-        if (hashArray[ind] == NULL) { //this entry points to null, we can use it for our new item
-            hashArray[ind] = new_item;
-            hashArray[ind]->data.indice = ind;
-            counter++;
-            return counter;
-        }
-        else if (key_to_int(hashArray[ind]->key) == key_int) { //this entry is being used and equals the key being read in
-            hashArray[ind]->data.num_occurrences = hashArray[ind]->data.num_occurrences + 1;
-            break;
-        }
+    while(hashArray[ind] != NULL) {
+        //go to next cell
         ++ind;
+
+        //wrap around the table
+        ind %= SIZE;
     }
-    return counter;
+	
+   hashArray[ind] = new_item;
 }
 
 
@@ -87,10 +81,24 @@ void print() {	//prints out the hash table in a sorted manner
     int i;
     for (i = 0; i < SIZE; i++) {
         if (hashArray[i] != NULL) {
-            printf("%s->%d\n", hashArray[i]->key, hashArray[i]->data.num_occurrences);
+            printf("%s->%d,%d\n", hashArray[i]->key, hashArray[i]->data.num_occurrences,hashArray[i]->data.indice);
         }
     }
 }
+
+void swap(int ind1, int ind2){
+    hashBlock * temp;
+    if ((hashArray[ind1] == NULL) || (hashArray[ind2] == NULL)){
+        return;
+    }
+    else {
+        temp = hashArray[ind1];
+        hashArray[ind1] = hashArray[ind2];
+        hashArray[ind2] = temp;
+    } 
+}
+
+
 
 int comp (const hashBlock * ele1, const hashBlock * ele2){
     if (ele1->data.num_occurrences > ele2->data.num_occurrences){
@@ -101,26 +109,45 @@ int comp (const hashBlock * ele1, const hashBlock * ele2){
     }
     else{
         if (ele1->data.indice > ele2->data.indice){
-            return 1;
+            return -1;
         }
         else if (ele2->data.indice > ele1->data.indice){
-            return -1;
+            return 1;
         }
         else{
             return 0;
         }
     }
 }
+
+void sort(){
+    int i,j,c,k;
+    
+    for (i=0; i<SIZE; i++){
+        if (hashArray[i] != NULL){
+            for (j=0; j<SIZE; j++){
+                if (hashArray[j] != NULL){ //two non-null entries have been found
+                    if (comp(hashArray[i],hashArray[j]) == 1){
+                        swap(i,j);
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main() {
     //struct hashBlock* hashArray;
     int i;
   //  int counter = 0;
     hashBlock * item;
+    data_t data;
 
     char c[5];
-    int value = 0;
+    int ind = 0;
 
     while (!feof(stdin)){
+        ind++;
         memset(c, 0, sizeof(c)); //clear the previous contents of c
 
         scanf("%c",&c[0]);
@@ -140,13 +167,19 @@ int main() {
 
         }
         else { //the character doesn't exist yet
-           // insert(c, 1, counter);
-            value = value + insert(c, 1);
+            data.num_occurrences = 1;
+            data.indice = ind;
+            insert(c, &data);
+            //value = value + insert(c, 1);
         }
 
     }
     //  bubbleSort(hashArray);
-    qsort(hashArray, value, sizeof(hashBlock), comp);
+    //print();
+    for (i=0; i<ind;i++){
+        sort();
+    }
+    //qsort(hashArray, value, sizeof(hashBlock), comp);
     print();
 }
 
